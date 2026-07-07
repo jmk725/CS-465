dd9e9052ac8812fe97e7a1b1f3c6838028d1528f
Jean Karst <jmk725@gmail.com>
2026-07-07 16:16:03 -0500

    Completed Module 2 MVC architecture

---
+++ app_server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+const ctrlMain = require('../controllers/travlr');
+
+/* GET home page. */
+router.get('/', ctrlMain.travel);
+router.get('/travel', ctrlMain.travel);
+
+module.exports = router;
