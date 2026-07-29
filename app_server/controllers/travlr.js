fa933bec584973072ac4d6278d764e7dab4cc9ee
Jean Karst <jmk725@gmail.com>
2026-07-29 12:39:00 -0500

    Complete Module 5 REST API refactor

--- app_server/controllers/travlr.js
+++ app_server/controllers/travlr.js
@@ -1,16 +1,43 @@
-var fs = require('fs');
+const tripsEndpoint = 'http://localhost:3000/api/trips';
 
-var trips = JSON.parse(
-    fs.readFileSync('./data/trips.json', 'utf8')
-);
+const options = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json'
+  }
+};
 
-const travel = (req, res) => {
-    res.render('travlr', {
+// GET travel view
+const travel = async (req, res) => {
+  await fetch(tripsEndpoint, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`API request failed with status ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .then(json => {
+      let message = null;
+
+      if (!(json instanceof Array)) {
+        message = 'API lookup error';
+        json = [];
+      } else if (!json.length) {
+        message = 'No trips exist in our database!';
+      }
+
+      res.render('travlr', {
         title: 'Travlr Getaways',
-        trips
+        trips: json,
+        message
+      });
+    })
+    .catch(err => {
+      res.status(500).send(err.message);
     });
 };
 
 module.exports = {
-    travel
+  travel
 };
